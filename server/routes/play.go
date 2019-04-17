6dbfea3470eed3d84287ba170d88b24e1a251f0e
alfaix <arsen.kitov@gmail.com>
2019-04-17 10:44:07 +0300

    temporary origin fix

--- server/routes/play.go
+++ server/routes/play.go
@@ -20,6 +20,9 @@ type SinglePlayHandler struct{}
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func (h *SinglePlayHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
