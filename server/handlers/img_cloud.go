3d0e58ef0a0e9893bedc790d0ab9c3afc2bb2948
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-04-19 16:13:05 +0300

    implemented delete image function for AWS

--- server/handlers/img_cloud.go
+++ server/handlers/img_cloud.go
@@ -3,11 +3,17 @@ package handlers
 import (
 	"bytes"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"image"
 	"image/jpeg"
 	"io"
+)
+
+const (
+	imageFolder = "img/"
 )
 
 // The basket that we use
@@ -16,9 +22,10 @@ var (
 	region     = "eu-north-1"
 )
 
-// Singleton
+// Singletons
 // Initialized once in init() function
 var uploader *s3manager.Uploader
+var svc *s3.S3
 
 // Initializing an uploader with the session and default options
 func init() {
@@ -26,11 +33,15 @@ func init() {
 	// and that the SDK will use to load
 	// credentials from environment variables
 	sess := session.Must(session.NewSession(&aws.Config{
-		Region:      aws.String(region),
+		Region: aws.String(region),
 	}))
 
 	// Create an uploader with the session and default options
 	uploader = s3manager.NewUploader(sess)
+
+	// Initialize instance of the S3 client with a session
+	// Needed for init BatchDeleteIterator
+	svc = s3.New(sess)
 }
 
 // Convert golang image.Image to bytes
@@ -49,7 +60,7 @@ func saveObject(keyName string, r io.Reader) error {
 	// Upload input parameters
 	upParams := &s3manager.UploadInput{
 		Bucket: &bucketName,
-		Key:    aws.String("img/" + keyName),
+		Key:    aws.String(imageFolder + keyName),
 		Body:   r,
 	}
 
@@ -72,6 +83,25 @@ func SaveImage(img image.Image, name string) error {
 	err = saveObject(name, buf)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// Delete image from AWS bucket
+func DeleteImage(img image.Image, name string) error {
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(imageFolder + name),
+	}
+
+	_, err := svc.DeleteObject(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			return aerr
+		} else {
+			return err
+		}
 	}
 
 	return nil
